4ce450548d12259c48db7a193466b508b2b171ff
Yahya Abunada <zayns1914@gmail.com>
2024-06-24 23:36:03 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ import 'package:trustedtallentsvalley/service_locator.dart';
 
 import 'fetures/Home/Providers/home_provider.dart';
 //
-//
 
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
